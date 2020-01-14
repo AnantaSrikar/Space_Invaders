7fbf8fcf0d6d1bc5f2025cba7f910b75299ee7fb
Ananta Srikar Puranam <srikarananta01@gmail.com>
2020-01-14 12:44:51 +0530

    Added player object

--- space_invaders.py
+++ space_invaders.py
@@ -9,13 +9,23 @@ pygame.display.set_caption('Space Invaders')
 icon = pygame.image.load('res/rocket.png')
 pygame.display.set_icon(icon)
 
+#player
+playerImage = pygame.image.load('res/spaceship.png')
+playerPos = (370, 480) # (X, Y)
+
+def player():
+    screen.blit(playerImage, playerPos) #draws the player
+
 running = True
 
 #game loop
 while(running):
+
+    screen.fill((0,0,0)) #(R, G, B)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT: #quits game when close button is pressed
             running = False
 
-    screen.fill((0,0,0)) #(R, G, B)
+    player()
     pygame.display.update()
