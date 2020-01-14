70990d70cf35b4fca54cc644309247325d7be355
Ananta Srikar <srikarananta01@gmail.com>
2020-01-14 09:10:05 +0530

    Added quitin when pressed cross

--- init.py
+++ init.py
@@ -3,3 +3,11 @@ import pygame
 pygame.init()
 
 screen = pygame.display.set_mode((800,600))
+
+running = True
+
+#game loop
+while(running):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: #quits game when close button is pressed
+            running = False
