aeb8c43c132f4636525032005449e37c0c5ad464
Ananta Srikar Puranam <srikarananta01@gmail.com>
2020-01-14 16:57:19 +0530

    Added movement of spaceship

--- space_invaders.py
+++ space_invaders.py
@@ -11,10 +11,15 @@ pygame.display.set_icon(icon)
 
 #player
 playerImage = pygame.image.load('res/spaceship.png')
-playerPos = (370, 480) # (X, Y)
 
-def player():
-    screen.blit(playerImage, playerPos) #draws the player
+playerX = 370
+playerY = 480
+
+playerX_change = 0
+playerY_change = 0
+
+def player(x, y):
+    screen.blit(playerImage, (x, y)) #draws the player
 
 running = True
 
@@ -24,8 +29,21 @@ while(running):
     screen.fill((0,0,0)) #(R, G, B)
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: #quits game when close button is pressed
+        if (event.type == pygame.QUIT): #quits game when close button is pressed
             running = False
+        if (event.type == pygame.KEYDOWN):
 
-    player()
+            if (event.key == pygame.K_LEFT):
+                playerX_change = -0.3
+
+            if (event.key == pygame.K_RIGHT):
+                playerX_change += 0.3
+
+        if (event.type == pygame.KEYUP):
+            if (event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT):
+                playerX_change = 0
+            
+    playerX += playerX_change
+
+    player(playerX, playerY)
     pygame.display.update()
